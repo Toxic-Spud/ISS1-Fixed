8a8d836c6f162d78ab0b5f98d0a3637d770e8cc9
Ryan <ryan.mballard@outlook.com>
2025-04-29 13:14:51 +0100

    Update installtpm.sh

--- installtpm.sh
+++ installtpm.sh
@@ -24,11 +24,9 @@ make
 sudo make install
 cd ..
 rm -rf swtpm/ libtpms/
-swtpm_setup --tpm2 --tpmstate dir=/home/ryan/mytpm --createek --create-ek-cert --create-platform-cert --lock-nvra
-swtpm socket --tpmstate dir=./mytpm  --ctrl type=tcp,port=2322 --server type=tcp,port=2321 --flags not-need-init --daemon
 sudo apt install python3-tpm2-pytss
 sudo apt install libtss2-dev tpm2-tools swig pkg-config python3-dev
 sudo apt install python3.12-venv
 python3 -m venv --system-site-packages .linuxenv
 source ./.linuxenv/bin/activate
-pip install -r final_requirements.txt 
+pip install -r reqs.txt
